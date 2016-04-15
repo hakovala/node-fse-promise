ce31512cdb4332faf503913bd73144d775e975ac
Hugo Armstrong <hugo.armstrong@gmail.com>
2016-04-15 17:07:06 +1000

    Add exists (#1)
    Add missing exists method as a special case

--- index.js
+++ index.js
@@ -31,6 +31,7 @@ const METHODS = [
 	'chmod',
 	'chown',
 	'close',
+	'exists',
 	'fchmod',
 	'fchown',
 	'fdatasync',
@@ -81,10 +82,10 @@ function callCallback(fn, args) {
 function callPromise(fn, args) {
 	return new Promise((resolve, reject) => {
 		// promisified callback
-		function callback(err) {
-			if (err) return reject(err);
-			let args = Array.prototype.slice.call(arguments, 1);
-			return resolve(args.length <= 1 ? args[0] : args);
+		function callback(err,other) {
+			if (err && !(typeof err === 'boolean')) return reject(err);
+			let args = arguments;
+			return resolve(args.length <= 1 ? args[0] : Array.prototype.slice.call(args,1));
 		}
 		fn.apply(null, args.concat([callback]));
 	});
